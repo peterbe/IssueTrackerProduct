7d88a0b90036ad85cebb6b58d6a1098a3436a7b1
Peter Bengtsson <mail@peterbe.com>
2009-04-27 10:49:46 +0000

    Added datepicker for due_date feature

---
+++ IssueTrackerProduct/Datepicker.py
@@ -0,0 +1,26 @@
+
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
+
+from Constants import DEFAULT_DATEPICKER_OPTIONS
+
+class DatepickerBase:
+    
+    security = ClassSecurityInfo()
+    
+    def getDatepickerOptions(self):
+        return getattr(self, 'datepicker_options', DEFAULT_DATEPICKER_OPTIONS)
+    
+    
+    def manage_saveDatepickerOptions(self, datepicker_options, REQUEST=None):
+        """save the new datepicker_options"""
+        self.datepicker_options = datepicker_options.strip()
+        
+        if REQUEST is not None:
+            url = self.getRootURL()+'/manage_DatepickerManagementForm'
+            url += '?manage_tabs_message=Options+saved.'
+            REQUEST.RESPONSE.redirect(url)
+            
+    
+    
+InitializeClass(DatepickerBase)    
