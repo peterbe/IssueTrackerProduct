9fd423a25aba94f53a30752f40e5d788860bfee7
Suresh V <suresh@grafware.com>
2013-07-21 23:58:20 +0530

    fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'IssueTrackerProduct'
-version = '0.14.2.dev0'
+version = '0.14.2dev0'
 
 setup(name='IssueTrackerProduct',
       version=version,
