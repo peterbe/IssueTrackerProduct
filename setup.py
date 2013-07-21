e80552ab763c6f02670a961c113e0b1209de61fc
Suresh V <suresh@grafware.com>
2013-07-21 22:38:56 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+name = 'IssueTrackerProduct'
+version = '0.14dev'
+
+setup(name='IssueTrackerProduct',
+      version=version,
+      description="Bug/issue tracker for Zope2.",
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope tracker issue bug',
+      author='Peter Bengtsson',
+      author_email='mail@peterbe.com',
+      url='https://github.com/sureshvv/IsssueTrackerProduct',
+      license='GPL',
+      packages=['IssueTrackerProduct',
+                'IssueTrackerMassContainer',
+                'IssueTrackerOpenID',
+               ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+)
